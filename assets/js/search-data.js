1300ce243b301f32fd6cfa94e4a1f732473ed8ec
ignacioperezrjs <69607126+ignacioperezrjs@users.noreply.github.com>
2025-04-07 02:55:30 +0000

    Deploying to gh-pages from @ ignacioperezrjs/ignacioperezrjs.github.io@521ad188c2db953edc9664486dbd657a3b653114 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "Teaching",
-          description: "A summary of my teaching experience and courses.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "dropdown-bookshelf",
               title: "Bookshelf",
               description: "",
@@ -45,11 +38,18 @@ ninja.data = [{
                 window.location.href = "/books/";
               },
             },{id: "dropdown-read-papers",
-              title: "Read papers",
+              title: "Read Papers",
               description: "",
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/blog/";
+              },
+            },{id: "dropdown-teaching-experience",
+              title: "Teaching experience",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/teaching/";
               },
             },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
       
@@ -60,17 +60,6 @@ ninja.data = [{
       handler: () => {
         
           window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
     },{id: "books-american-gods",
