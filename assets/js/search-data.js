6aaa878895d3cde45c66bafa2c7a8527f0a2a3cc
ignacioperezrjs <69607126+ignacioperezrjs@users.noreply.github.com>
2025-04-07 02:36:00 +0000

    Deploying to gh-pages from @ ignacioperezrjs/ignacioperezrjs.github.io@caa60225308767a8036921fade2d5b727d6625da 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -96,9 +96,9 @@ ninja.data = [{
           description: "Collaborated on a 1.3 GW green hydrogen project at CISC SpA, enhancing document management and efficiency in eolic energy generation.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/HNH_project/";
-            },},{id: "projects-trefimet-induction-lance-heater",
-          title: 'Trefimet Induction Lance Heater',
-          description: "Contributed to design a safe, automated induction heating system for industrial lances, replacing hazardous manual processes in mining operations.",
+            },},{id: "projects-thermal-lance-automation",
+          title: 'Thermal Lance Automation',
+          description: "Automated heating system for industrial thermal lances to eliminate worker risk in mining operations.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Trefimet/";
             },},{
